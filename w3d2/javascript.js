d07b70393905931a96de1126049bc43e17267f38
Najed-Hatamleh <86274093+Najed-Hatamleh@users.noreply.github.com>
2021-07-07 11:32:06 -0500

    Placed all JS function inside the block, to keep clean the global object.

--- w3d2/javascript.js
+++ w3d2/javascript.js
@@ -27,39 +27,39 @@ $(function() {
         $(this).css("opacity", '100%');
         clearInterval(circuleOpacityTimer);
     });
-});
 
-function createCircule() {
-    let createdElement;
-    let circulesWidth = $('.circules').width();
+    function createCircule() {
+        let createdElement;
+        let circulesWidth = $('.circules').width();
 
-    createdElement = $("<div class='circule'><div>");
-    createdElement.css("background-color", getRandomColor());
-    createdElement.css("left", Math.floor(circulesWidth * Math.random()));
-    createdElement.appendTo(".circules");
+        createdElement = $("<div class='circule'><div>");
+        createdElement.css("background-color", getRandomColor());
+        createdElement.css("left", Math.floor(circulesWidth * Math.random()));
+        createdElement.appendTo(".circules");
 
-    let circuleCreationTimer = setInterval(grow, parseInt($('#set-growth-rate').val()), createdElement);
-    createdElement.click(() => {
-        createdElement.remove();
-        clearInterval(circuleCreationTimer);
-    });
-}
-
-function grow(element) {
-    element.css('height', element.height() + parseInt($('#set-growth-amount').val()));
-    element.css('width', element.width() + parseInt($('#set-growth-amount').val()));
-}
-
-function opactiy(element) {
-    element.css("opacity", (index, value) => value * 0.8);
-}
-
-// Some guidunce from the internet, refereance: https://stackoverflow.com/questions/1484506/random-color-generator
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        let circuleCreationTimer = setInterval(grow, parseInt($('#set-growth-rate').val()), createdElement);
+        createdElement.click(() => {
+            createdElement.remove();
+            clearInterval(circuleCreationTimer);
+        });
     }
-    return color;
-}
+
+    function grow(element) {
+        element.css('height', element.height() + parseInt($('#set-growth-amount').val()));
+        element.css('width', element.width() + parseInt($('#set-growth-amount').val()));
+    }
+
+    function opactiy(element) {
+        element.css("opacity", (index, value) => value * 0.8);
+    }
+
+    // Some guidunce from the internet, refereance: https://stackoverflow.com/questions/1484506/random-color-generator
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
